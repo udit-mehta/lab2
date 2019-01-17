5ae4694783f158599cefe64cddf15c7e7bfdfdf9
udit034 <udit.mehata@gmail.com>
2019-01-17 10:28:18 -0800

    added Javascript!

---
+++ static/js/introHCI.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+	});
+
+	function projectClick(e) { 
+	    // prevent the page from reloading      
+	    e.preventDefault();
+	    // In an event handler, $(this) refers to      
+	    // the object that triggered the event      
+	    $(this).css("background-color", "#7fff00");
+	};
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+}
